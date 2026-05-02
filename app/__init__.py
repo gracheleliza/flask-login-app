3dba203cad610bc307511852238edc816fd667ad
Grachel Ching <grachelelizaching@gmail.com>
2026-05-02 10:04:59 +0800

    Initial commit: Flask login app structure

---
+++ app/__init__.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
+from app.config import Config
+
+db = SQLAlchemy()
+login_manager = LoginManager()
+bcrypt = Bcrypt()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    login_manager.init_app(app)
+    bcrypt.init_app(app)
+
+    login_manager.login_view = 'auth.login'
+
+    from app.controllers.auth_controller import auth
+    app.register_blueprint(auth)
+
+    return app
