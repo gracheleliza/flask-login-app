e33e5be9f2a10eb00152b034d0090b3cb13630e8
Grachel Ching <grachelelizaching@gmail.com>
2026-05-02 11:17:24 +0800

    UI improvements + layout fix + database config setup

--- app/config.py
+++ app/config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = 'your-secret-key'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///site.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
